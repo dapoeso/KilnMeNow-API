34f00c2a57ab6def80376896b51bb92726a20ac0
Dami Apoeso <damiapoeso@damis-air.lan>
2023-12-04 10:30:11 -0500

    added routes

---
+++ README.md
@@ -0,0 +1,7 @@
+# KilnMeNow-API
+
+In order to use the API locally you will need to instal mysql connector and create a local DB server with the following parameters:
+- host: "localhost",
+- user: "root",
+- password: "kilnmenow",
+- database: "dev",
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
-import mysql from 'mysql';
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mysql from "mysql";
 
 const app = express();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password:"kilnmenow",
-    database:"dev"
+  host: "localhost",
+  user: "root",
+  password: "kilnmenow",
+  database: "dev",
 });
 
 // if you have auth problem
@@ -23,57 +23,173 @@ app.use(cors());
 
 // ROUTES
 app.get("/", (req, res) => {
-    res.json("hello world");
+  res.json("hello world");
 });
 
 app.get("/kilns", (req, res) => {
-    const q = `SELECT * FROM dev.Kiln k 
-    INNER JOIN dev.Address a ON k.address_id = a.id 
-    INNER JOIN dev.Photo p on k.id = p.kiln_id`;
-    db.query(q, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
+  const q = `SELECT * FROM dev.Kiln k 
+    INNER JOIN dev.Address a ON k.address_id = a.address_id 
+    INNER JOIN dev.Photo p on k.kiln_id = p.kiln_id`;
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
 app.get("/kilns/:id", (req, res) => {
-    console.log(req.params);
-    const id = req.params.id;
-    const q = `SELECT * FROM dev.Kiln k INNER JOIN dev.Address a ON k.address_id = a.id WHERE k.id = ${id}`;
-    db.query(q, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    });
+  console.log(req.params);
+  const id = req.params.id;
+  const q = `SELECT * FROM dev.Kiln k 
+    INNER JOIN dev.Address a ON k.address_id = a.address_id 
+    INNER JOIN dev.Photo p on k.kiln_id = p.kiln_id
+    WHERE k.kiln_id = ${id}`;
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
-app.get('/address', (req, res) => {
-    const query = "SELECT * FROM dev.Address"
-    db.query(query, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
+app.get("/address", (req, res) => {
+  const query = "SELECT * FROM dev.Address";
+  db.query(query, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  const q = `INSERT INTO dev.User (user_id, email) VALUES (?);`;
+  const { id, email } = req.body;
+  const values = [id, email];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    console.log(data);
+    return res.json(data);
+  });
+});
+
+app.get("/users/:email", (req, res) => {
+  console.log(req.params);
+  const email = req.params.email;
+  const q = `SELECT * FROM dev.User WHERE email="${email}"`;
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.patch("/users/:email", (req, res) => {
+  console.log(req.body);
+  const { firstName, lastName, phone } = req.body;
+  const email = req.params.email;
+  const q = `UPDATE dev.User SET first_name="${firstName}", last_name="${lastName}", phone="${phone}" WHERE email="${email}"`;
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    console.log(data);
+    return res.json(data);
+  });
+});
+
+app.post("/address", (req, res) => {
+    const q = `INSERT INTO dev.Address (address_1, address_2, city, state, postal, country, user_id, is_billing) VALUES (?);`;
+    const {address1, address2, city, state, zip, country, userId, isBilling } = req.body;
+    const values = [address1, address2, city, state, zip, country, userId, isBilling];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.json(err);
+      return res.json({id: data.insertId});
     });
+    // const q2 = `SELECT * FROM dev.Address WHERE user_id=${userId} ORDER BY address_id DESC LIMIT 1`;
+    // db.query(q2, (err, data) => {
+    //   if (err) return res.json(err);
+    //   console.log(data);
+    //   return res.json(data);
+    // });
 });
 
 app.post("/kilns", (req, res) => {
-    const q = "(id, host_id, title, description, user_id, is_available, created_at, size, cone, address_id, latitude, longitude, turnaround, listing_price) VALUES"
-    const {id, user_id, title, description, is_available, size, cone, address_id, listing_price, kiln_type} = req.body;
-    const values = [
-        id,
-        user_id,
-        title,
-        description,
-        is_available,
-        size,
-        cone,
-        address_id,
-        listing_price,
-        kiln_type
-    ];
-    db.query(q, [values], (err, data) => {
-        if (err) return res.json(err);
-        return res.json("Kiln Successfully Registered");
-    })
+    console.log(req.body);
+  const q = `INSERT INTO dev.Kiln (user_id, title, description, is_available, size, cone, address_id, latitude, longitude, turnaround, listing_price) VALUES (?);`;
+  let kilnId
+  const {
+    userId,
+    name,
+    description,
+    isAvailable,
+    size,
+    cone,
+    addressId,
+    price,
+    latitude,
+    longitude,
+    turnaround,
+    url,
+  } = req.body;
+  const values = [
+    userId,
+    name,
+    description,
+    isAvailable,
+    size,
+    cone,
+    addressId,
+    latitude,
+    longitude,
+    turnaround,
+    price,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    console.log(data);
+    kilnId = data.insertId;
+    console.log('kiln id', kilnId);
+    const q2 = `INSERT INTO dev.Photo (kiln_id, url) VALUES (?);`;
+    const photoValues = [kilnId, url];
+        db.query(q2, [photoValues], (err, data) => {
+            if (err) return res.json(err);
+            console.log(data);
+            return res.json(data);
+        });
+  });
 });
+
+app.post("/reservation", (req, res) => {
+    const q = `INSERT INTO dev.Reservation (user_id, kiln_id, price, status) VALUES (?);`;
+    const {userId, kilnId, price, status } = req.body;
+    const values = [userId, kilnId, price, status];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+    });
+});
+
+app.get("/reservation/:id", (req, res) => {
+    const q = `SELECT * from dev.Reservation r 
+    INNER JOIN dev.Kiln k on r.kiln_id = k.kiln_id
+    INNER JOIN dev.Photo p on r.kiln_id = p.kiln_id 
+    WHERE r.user_id = '${req.params.id}'`;
+    db.query(q, (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+    });
+});
+
+app.patch("/reservation/:id", (req, res) => {
+    const q = `UPDATE dev.Reservation SET is_complete=1 WHERE id=${req.params.id}`;
+    db.query(q, (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+    });
+});
+
+app.patch("/cancel-reservation/:id", (req, res) => {
+    const q = `DELETE FROM dev.Reservation WHERE id=${req.params.id}`;
+    db.query(q, (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+    });
+});
+
 
 // app.use(compression());
 // app.use(cookieParser());
@@ -81,5 +197,5 @@ app.post("/kilns", (req, res) => {
 // const server = http.createServer(app);
 
 app.listen(6969, () => {
-    console.log('Server running on http://localhost:6969/')
+  console.log("Server running on http://localhost:6969/");
 });
